c57cd84ed103586cceb2e0216eabb0a0782e4e43
Tony Thai <tony.thai@illuminos.sg>
2020-08-17 17:58:46 +0700

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd223886bc5a83217e9152051b4c84e39'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '238c92e4-a6b5-4a5b-a91b-49dbc28abd49'
+};
